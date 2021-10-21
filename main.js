a6b6b1560e2778082626506a63454da11828d59b
jisu yeo <jisuyeo@jisus-air.myfiosgateway.com>
2021-10-21 14:16:36 -0400

    js fix

--- main.js
+++ main.js
@@ -69,21 +69,36 @@ const workBtnContainer = document.querySelector('.work__categories')
 const projectContainer = document.querySelector('.work__projects')
 const projects = document.querySelectorAll('.project')
 
+
+
+
+
 workBtnContainer.addEventListener('click', (e)=>{
     const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter
     if(filter == null) {
         return
     }
-    projectContainer.classList.add('anim-out')
-    projects.forEach((project) =>{
-        console.log(project.dataset.type);
-        if(filter === '*' || filter === project.dataset.type) {
-            project.classList.remove('invisible')
-        } else {
-            project.classList.add('invisible')
-        }
-    })
 
+// Remove selectiom from orevious item and select the new
+const active = document.querySelector('.category__btn.selected')
+active.classList.remove('selected')
+const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode
+target.classList.add('selected')
+
+
+projectContainer.classList.add('anim-out');
+setTimeout(() => {
+    projects.forEach((project) => {
+    console.log(project.dataset.type);
+    if (filter === '*' || filter === project.dataset.type) {
+        project.classList.remove('invisible');
+    } else {
+        project.classList.add('invisible');
+    }
+    });
+    projectContainer.classList.remove('anim-out');
+}, 300);
+    
     
     // console.log(filter);
 })
